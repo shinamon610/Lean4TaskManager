1c11068038608d5eb9bb56d2ffd25d35275868d9
shinamon <76248816+shinamon610@users.noreply.github.com>
2025-04-02 16:19:17 +0900

    fix flake

--- flake.nix
+++ flake.nix
@@ -6,12 +6,6 @@
   outputs = { self, nixpkgs }:
     let pkgs = import nixpkgs { system = "x86_64-linux"; };
     in {
-      devShell.x86_64-linux = pkgs.mkShell {
-        buildInputs = [
-          pkgs.elan
-          pkgs.gcalcli
-          pkgs.python312
-        ]; # elanを使うようにしているだけだけど、なぜかelan経由でinstallしたlakeもstore objectになっていて、この環境だけ存在するようになっている
-      };
+      devShell.x86_64-linux = pkgs.mkShell { buildInputs = [ pkgs.elan ]; };
     };
 }
